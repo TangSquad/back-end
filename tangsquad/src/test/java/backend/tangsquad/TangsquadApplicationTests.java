ebd515e9fa8cb96870b7e4cef043ef052f1420b6
junnie082 <junnie082@gmail.com>
2024-07-06 13:00:50 +0900

    feat: Initialize Tang Squad Project

---
+++ tangsquad/src/test/java/backend/tangsquad/TangsquadApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.tangsquad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TangsquadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
