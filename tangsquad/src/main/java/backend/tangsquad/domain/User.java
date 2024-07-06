7779d5a139b42adeefe86005dc0789978c4aa86a
yseokkkkkk <yunseok0304@gmail.com>
2024-07-06 19:39:19 +0900

    feat: validation dependency 추가, UserCredential 삭제 및 User와 통합

--- tangsquad/src/main/java/backend/tangsquad/domain/User.java
+++ tangsquad/src/main/java/backend/tangsquad/domain/User.java
@@ -1,9 +1,12 @@
 package backend.tangsquad.domain;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -11,6 +14,7 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @Table(name="users")
+@EntityListeners(AuditingEntityListener.class)
 public class User {
 
     @Id
@@ -29,10 +33,13 @@ public class User {
     // 전화번호
     @Column(nullable = false, unique = true) private String phone;
 
+    // 비밀번호(해시)
+    @Column private String password;
+
     // 로그인 방법(일반, 카카오, 구글)
     @Column(nullable = false) private String loginMethod;
 
     // 가입 날짜
     @CreatedDate
-    @Column private LocalDateTime createdAt;
+    @Column(updatable = false) private LocalDateTime createdAt;
 }
