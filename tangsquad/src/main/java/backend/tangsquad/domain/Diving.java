c3190384c92d26da319148006fb9059eedebe9bb
junnie082 <junnie082@gmail.com>
2024-08-06 00:26:39 +0900

    feat: Diving.java

--- tangsquad/src/main/java/backend/tangsquad/domain/Diving.java
+++ tangsquad/src/main/java/backend/tangsquad/domain/Diving.java
@@ -1,2 +1,19 @@
-package backend.tangsquad.domain;public class Diving {
+package backend.tangsquad.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name="diving")
+public class Diving {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // figma 작업 완료 후 개발
+
 }
