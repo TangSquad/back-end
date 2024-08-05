78bf11d859415598568f91841959053673e94dad
junnie082 <junnie082@gmail.com>
2024-08-06 00:18:25 +0900

    fix: changed Moim.java

---
+++ tangsquad/src/main/java/backend/tangsquad/domain/Diving.java
@@ -0,0 +1,2 @@
+package backend.tangsquad.domain;public class Diving {
+}
--- tangsquad/src/main/java/backend/tangsquad/domain/Moim.java
+++ tangsquad/src/main/java/backend/tangsquad/domain/Moim.java
@@ -1,144 +1,55 @@
 package backend.tangsquad.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
+@Entity
+@Getter
+@Setter
+@Table(name="moim")
 public class Moim {
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
+    @Id
     private Long id;
+
+    @Column
     private String moimOwner;
 
-    private String[] moimMembers;
-
+//    private String[] moimMembers;
+    @Column
+    private String moimMembers;
+    @Column
     private LocalDateTime startDate;
+    @Column
     private LocalDateTime endDate;
+    @Column
     private String moimName;
+    @Column
     private String moimIntro;
+    @Column
     private String moimContents;
-
+    @Column
     private Integer maxPeople;
-
+    @Column
     private Float price;
 
-    private String[] licenseLimit;
-
-    private String[] region;
-
+//    private String[] licenseLimit;
+    @Column
+    private String licenseLimit;
+//    private String[] region;
+    @Column
+    private String region;
+    @Column
     private String age;
 
-    private String[] mood;
+//    private String[] mood;
+    @Column
+    private String mood;
 
-    public Moim() {
-    }
-
-    public String getMoimOwner() {
-        return moimOwner;
-    }
-
-    public void setMoimOwner(String moimOwner) {
-        this.moimOwner = moimOwner;
-    }
-
-    public String[] getMoimMembers() {
-        return moimMembers;
-    }
-
-    public void setMoimMembers(String[] moimMembers) {
-        this.moimMembers = moimMembers;
-    }
-
-    public LocalDateTime getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(LocalDateTime startDate) {
-        this.startDate = startDate;
-    }
-
-    public LocalDateTime getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(LocalDateTime endDate) {
-        this.endDate = endDate;
-    }
-
-    public String getMoimName() {
-        return moimName;
-    }
-
-    public void setMoimName(String moimName) {
-        this.moimName = moimName;
-    }
-
-    public String getMoimIntro() {
-        return moimIntro;
-    }
-
-    public void setMoimIntro(String moimIntro) {
-        this.moimIntro = moimIntro;
-    }
-
-    public String getMoimContents() {
-        return moimContents;
-    }
-
-    public void setMoimContents(String moimContents) {
-        this.moimContents = moimContents;
-    }
-
-    public Integer getMaxPeople() {
-        return maxPeople;
-    }
-
-    public void setMaxPeople(Integer maxPeople) {
-        this.maxPeople = maxPeople;
-    }
-
-    public Float getPrice() {
-        return price;
-    }
-
-    public void setPrice(Float price) {
-        this.price = price;
-    }
-
-    public String[] getLicenseLimit() {
-        return licenseLimit;
-    }
-
-    public void setLicenseLimit(String[] licenseLimit) {
-        this.licenseLimit = licenseLimit;
-    }
-
-    public String[] getRegion() {
-        return region;
-    }
-
-    public void setRegion(String[] region) {
-        this.region = region;
-    }
-
-    public String getAge() {
-        return age;
-    }
-
-    public void setAge(String age) {
-        this.age = age;
-    }
-
-    public String[] getMood() {
-        return mood;
-    }
-
-    public void setMood(String[] mood) {
-        this.mood = mood;
-    }
 }
