857d61d15774ffe892550a9a04f6eafe189b7bb6
junnie082 <junnie082@gmail.com>
2024-08-14 13:25:32 +0900

    chores: added getId() in UserDetailsImpl

--- tangsquad/src/main/java/backend/tangsquad/auth/jwt/UserDetailsImpl.java
+++ tangsquad/src/main/java/backend/tangsquad/auth/jwt/UserDetailsImpl.java
@@ -30,4 +30,6 @@ public class UserDetailsImpl implements UserDetails {
     public String getUsername() {
         return user.getUsername();
     }
+
+    public Long getId() { return user.getId(); }
 }
