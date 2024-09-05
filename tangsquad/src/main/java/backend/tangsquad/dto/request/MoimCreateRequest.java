cd4f15fa4a2fbae4f4b8c41543543281effe6e31
junnie082 <junnie082@gmail.com>
2024-09-05 16:10:30 +0900

    feat: developed CRU of Moim

--- tangsquad/src/main/java/backend/tangsquad/dto/request/MoimCreateRequest.java
+++ tangsquad/src/main/java/backend/tangsquad/dto/request/MoimCreateRequest.java
@@ -1,10 +1,8 @@
 package backend.tangsquad.dto.request;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import backend.tangsquad.domain.User;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,28 +13,17 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 public class MoimCreateRequest {
-
-    private Long id;
-
-    private String moimOwner;
-
-    //    private String[] moimMembers;
-    private String moimMembers;
-    private LocalDateTime startDate;
-    private LocalDateTime endDate;
+    private Boolean anonymous;
     private String moimName;
     private String moimIntro;
-    private String moimContents;
-    private Integer maxPeople;
-    private Float price;
-
-    //    private String[] licenseLimit;
+    private String moimDetails;
+    private Long limitPeople;
+    private Long expense;
     private String licenseLimit;
-    //    private String[] region;
-    private String region;
-    private String age;
-
-    //    private String[] mood;
-    private String mood;
-
+    private String locationOne;
+    private String locationTwo;
+    private String locationThree;
+    private Long age;
+    private String moodOne;
+    private String moodTwo;
 }
