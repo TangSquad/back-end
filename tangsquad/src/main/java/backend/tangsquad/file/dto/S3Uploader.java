5c15caa6142bf77f67e08888e32e2b4d2f62ef4a
junnie082 <junnie082@gmail.com>
2024-09-10 15:07:19 +0900

    fix: added generatePresignedUrl()

--- tangsquad/src/main/java/backend/tangsquad/file/dto/S3Uploader.java
+++ tangsquad/src/main/java/backend/tangsquad/file/dto/S3Uploader.java
@@ -1,8 +1,8 @@
 package backend.tangsquad.file.dto;
 
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,46 +12,61 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URL;
+import java.util.Date;
 import java.util.Optional;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class S3Uploader {
-    private AmazonS3Client amazonS3Client;
+
+    private final AmazonS3 amazonS3Client;
 
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
+    public String upload(MultipartFile file, String dirName) throws IOException {
+        File localFile = convert(file).orElseThrow(() -> new IllegalArgumentException("Failed to convert MultipartFile to File"));
 
-    private String upload(File uploadFile, String dirName) {
-        String fileName = dirName + "/" + uploadFile.getName();
-        String uploadImageUrl = putS3(uploadFile, fileName);
+        String fileName = dirName + "/" + localFile.getName();
+        String uploadImageUrl = putS3(localFile, fileName);
 
-        removeNewFile(uploadFile);  // convert()함수로 인해서 로컬에 생성된 File 삭제 (MultipartFile -> File 전환 하며 로컬에 파일 생성됨)
+        removeNewFile(localFile); // Delete the local file after uploading
 
-        return uploadImageUrl;      // 업로드된 파일의 S3 URL 주소 반환
+        return uploadImageUrl; // Return the pre-signed URL
     }
 
     private String putS3(File uploadFile, String fileName) {
-        amazonS3Client.putObject(
-                new PutObjectRequest(bucket, fileName, uploadFile)
-                        .withCannedAcl(CannedAccessControlList.PublicRead)	// PublicRead 권한으로 업로드 됨
-        );
-        return amazonS3Client.getUrl(bucket, fileName).toString();
+        amazonS3Client.putObject(bucket, fileName, uploadFile);
+        return generatePresignedUrl(fileName);
+    }
+
+    private String generatePresignedUrl(String fileName) {
+        // Set the expiration time for the URL
+        Date expiration = new Date();
+        long expTimeMillis = expiration.getTime();
+        expTimeMillis += 1000 * 60 * 60; // 1 hour
+        expiration.setTime(expTimeMillis);
+
+        GeneratePresignedUrlRequest generatePresignedUrlRequest = new GeneratePresignedUrlRequest(bucket, fileName)
+                .withExpiration(expiration);
+
+        URL url = amazonS3Client.generatePresignedUrl(generatePresignedUrlRequest);
+        return url.toString();
     }
 
     private void removeNewFile(File targetFile) {
-        if(targetFile.delete()) {
-            log.info("파일이 삭제되었습니다.");
-        }else {
-            log.info("파일이 삭제되지 못했습니다.");
+        if (targetFile.delete()) {
+            log.info("File deleted successfully.");
+        } else {
+            log.warn("Failed to delete file.");
         }
     }
 
-    private Optional<File> convert(MultipartFile file) throws  IOException {
-        File convertFile = new File(file.getOriginalFilename()); // 업로드한 파일의 이름
-        if(convertFile.createNewFile()) {
+    private Optional<File> convert(MultipartFile file) throws IOException {
+        File convertFile = new File(file.getOriginalFilename());
+        if (convertFile.createNewFile()) {
             try (FileOutputStream fos = new FileOutputStream(convertFile)) {
                 fos.write(file.getBytes());
             }
