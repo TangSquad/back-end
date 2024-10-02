7066dfb8b41756d7dfda52a0b6e5d625e563926a
junnie082 <junnie082@gmail.com>
2024-10-03 05:46:25 +0900

    fix: Log 엔티티 생성, 불변의 정보는 Logbook에, 변하는 정보는 Log에 저장

--- tangsquad/src/main/java/backend/tangsquad/logbook/entity/Log.java
+++ tangsquad/src/main/java/backend/tangsquad/logbook/entity/Log.java
@@ -1,2 +1,47 @@
-package backend.tangsquad.logbook.entity;public class Log {
+package backend.tangsquad.logbook.entity;
+
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+public class Log {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @Column
+    private String viewSight;
+
+    @Column
+    private String tide;
+
+    @Column
+    private LocalDateTime startDiveTime;
+
+    @Column
+    private LocalDateTime endDiveTime;
+
+    @Column
+    private LocalDateTime timeDiffDive;
+
+    @Column
+    private Float avgDepDiff;
+
+    @Column
+    private Float maxDiff;
+
+    @Column
+    private Long startBar;
+
+    @Column
+    private Long endBar;
+
+    @Column
+    private Long diffBar;
+
 }
