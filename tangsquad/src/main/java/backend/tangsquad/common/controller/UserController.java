dfd96b786f075458ce49148e9d2630827c9e272c
junnie082 <junnie082@gmail.com>
2024-10-06 19:06:06 +0900

    refactor: Logbook 모든 API 1차 리팩토링

--- tangsquad/src/main/java/backend/tangsquad/common/controller/UserController.java
+++ tangsquad/src/main/java/backend/tangsquad/common/controller/UserController.java
@@ -10,22 +10,14 @@ import backend.tangsquad.common.service.AuthService;
 import backend.tangsquad.common.service.ProfileService;
 import backend.tangsquad.common.service.UserService;
 import backend.tangsquad.common.service.VerificationService;
-import backend.tangsquad.logbook.dto.request.LogCreateRequest;
-import backend.tangsquad.logbook.entity.Logbook;
-import backend.tangsquad.logbook.service.LogbookService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/auth")
--- tangsquad/src/main/java/backend/tangsquad/logbook/repository/LogbookRepository.java
+++ tangsquad/src/main/java/backend/tangsquad/logbook/repository/LogbookRepository.java
@@ -1,10 +1,9 @@
 package backend.tangsquad.logbook.repository;
 
 
+import backend.tangsquad.logbook.dto.request.LogbookRequest;
 import backend.tangsquad.logbook.entity.Logbook;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,7 +12,5 @@ import java.util.Optional;
 @Repository
 public interface LogbookRepository extends JpaRepository<Logbook, Long> {
     List<Logbook> findByUserId(Long userId);
-    @Query("SELECT l FROM Logbook l WHERE l.id = :logId AND l.user.id = :userId")
-    Optional<Logbook> findByIdAndUserId(@Param("logId") Long logId, @Param("userId") Long userId);
-
+    Optional<Logbook> findByIdAndUserId(Long logId, Long userId);
 }
