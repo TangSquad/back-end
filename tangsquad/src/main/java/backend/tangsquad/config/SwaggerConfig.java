0dcb99129dc01c5bd3ec4ccf755d55dab48bcd9c
seokkkkkk <tjr6613@seoultech.ac.kr>
2024-11-09 19:57:52 +0900

    feat: swagger production sever 추가

--- tangsquad/src/main/java/backend/tangsquad/config/SwaggerConfig.java
+++ tangsquad/src/main/java/backend/tangsquad/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package backend.tangsquad.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
@@ -12,6 +13,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
+        servers = {
+                @Server(url = "https://api.tangsquad.com", description = "Production Server"),
+                @Server(url = "http://localhost:8080", description = "Local Server")
+        },
         info = @Info(
                 title = "TangSquad API",
                 description = "TangSquad API Documentation",
